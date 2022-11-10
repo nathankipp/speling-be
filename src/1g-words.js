5eb23fb494c1ec3f7eb18c735af0ed2a458da6a0
Nathan Kipp <nskipp@gmail.com>
2022-11-09 22:14:08 -0600

    first

---
+++ src/1g-words.js
@@ -0,0 +1,24 @@
+const words = `
+the,like,has,came,all,been,
+can,go,put,gave,down,because,
+to,look,into,make,food,before,
+am,you,fast,time,good,friend,
+have,here,back,something,house,one,
+it,see,jump,give,how,little,
+at,my,just,come,new,could,
+do,on,off,what,night,too,
+and,going,pick,walk,now,two,
+big,they,stop,boy,or,does,
+in,but,that,eat,our,are,
+not,did,them,play,out,car,
+said,fun,then,rain,saw,girl,
+from,got,this,road,says,her,
+is,had,went,teach,school,there,
+get,him,when,old,brother,were,
+of,if,will,over,children,very,
+up,run,with,can’t,father,where,
+for,tell,want,don't,mother,who,
+he,us,was,I'm,sister,why`;
+
+const a = words.split(',').map(w => w.trim());
+export default a;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,143 @@
-import logo from './logo.svg';
+import React from 'react';
+import uniq from  'lodash/uniq';
 import './App.css';
+import words from './1g-words';
 
-function App() {
+const initialState = {
+  score: 0,
+  step: 1,
+  word: undefined,
+  choices: [],
+  ans: '',
+};
+
+const getWord = () => {
+  const idx = Math.floor(Math.random() * words.length);
+  return words[idx];
+}
+const AZ = 'abcdefghijklmnopqrstuvqxyz'.split('');
+
+const scoreBoard = score => <div className="score">Score: {score}</div>;
+
+const read = (state, next) => {
+  const { score, word } = state;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      {scoreBoard(score)}
+      <div className="read">
+        <div>{word}</div>
+        <button onClick={next}>Spell It!</button>
+      </div>
+    </React.Fragment>
   );
+};
+
+const spell = (state, print, remove, giveUp) => {
+  const { score, choices, ans } = state;
+  return (
+    <React.Fragment>
+    {scoreBoard(score)}
+    <div className="spell">
+      <div>&nbsp;{ans}&nbsp;</div>
+      <div>
+        {choices.sort().map(letter => (
+          <button key={letter} onClick={() => print(letter)}>{letter}</button>
+        ))}
+      </div>
+      <div>
+        <button className="remove" onClick={remove}> &lt; </button>
+        <button className="give-up" onClick={giveUp}> ? </button>
+      </div>
+    </div>
+    </React.Fragment>
+  );
+};
+
+const yes = (state) => {
+  const { score, ans } = state;
+  return (
+    <React.Fragment>
+    {scoreBoard(score)}
+    <div className="yes">
+      <div>&nbsp;{ans}&nbsp;</div>
+    </div>
+    </React.Fragment>
+  )
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
+
+  next(step) {
+    const { word } = this.state;
+    const choices = uniq(word.split(''));
+    while (choices.length < 12) {
+      const idx = Math.floor(Math.random() * 26);
+      const letter = AZ[idx];
+      if (!choices.includes(letter)) {
+        choices.push(letter);
+      }
+    }
+
+    this.setState(prevState => ({
+      ...this.state,
+      choices,
+      step: prevState.step + 1,
+    }));
+  }
+
+  print(char) {
+    const guess = this.state.ans.concat(char);
+    const step = guess === this.state.word ? 3 : 2;
+
+    this.setState({
+      ...this.state,
+      ans: guess,
+      step,
+    });
+
+    if (step === 3) {
+      setTimeout(() => this.setState({
+        ...this.state,
+        score: this.state.score + 1,
+        step: 1,
+        word: getWord(),
+        ans: ''
+      }), 2000);
+    }
+  }
+
+  remove() {
+    this.setState({
+      ...this.state,
+      ans: this.state.ans.replace(/.$/, ''),
+    });
+  }
+
+  giveUp() {
+    this.setState({
+      ...initialState,
+      word: getWord(),
+    });
+  }
+
+  componentDidMount() {
+    this.setState({
+      ...this.state,
+      word: getWord(),
+    });
+  }
+
+  render() {
+    switch(this.state.step) {
+      case 1: return read(this.state, this.next.bind(this));
+      case 2: return spell(this.state, this.print.bind(this), this.remove.bind(this), this.giveUp.bind(this));
+      case 3: return yes(this.state);
+    }
+  }
 }
 
 export default App;
