59ce331d7b2befcb747adddaff2ed6c3afa518f1
Nathan Kipp <nskipp@gmail.com>
2022-11-11 08:55:49 -0600

    reset button

--- src/App.js
+++ src/App.js
@@ -4,14 +4,29 @@ import difference from 'lodash/difference';
 import './App.css';
 import words from './1g-words';
 
-const initialState = {
+const AZ = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+const STORAGE_KEYS = {
+  high: 'speling-be',
+  spelled: 'spelled',
+};
+
+const INITIAL_STATE = {
   step: 0,
   score: 0,
   word: undefined,
   choices: [],
   ans: '',
-  high: null,
+  high: 0,
   spelled: [],
+};
+
+const reset = () => {
+  if (window.confirm("Reset words and high score?")) {
+    window.localStorage.setItem(STORAGE_KEYS.high, 0);
+    window.localStorage.setItem(STORAGE_KEYS.spelled, '[]');
+    window.location.reload();
+  }
 };
 
 const getWord = (spelled = []) => {
@@ -19,92 +34,99 @@ const getWord = (spelled = []) => {
   const idx = Math.floor(Math.random() * unspelled.length);
   return unspelled[idx];
 }
-const AZ = 'abcdefghijklmnopqrstuvqxyz'.split('');
 
 const scoreBoard = (score, high) => (
   <div className="score">
     <div>Score: {score}</div>
-    {high > 0 ? <div>High: {high}</div> : null}
+    <div>High: {high}</div>
   </div>
 );
 
-const start = (state, start) => (
-  <div className="app go">
-    {scoreBoard(state.score, state.high)}
-    <div>
-      Get ready to spell!
-      <div className="status">{difference(words, state.spelled).length} of {words.length} words left to solve</div>
-    </div>
-    <div><button className="go" onClick={start}>Go</button></div>
-  </div>
-);
-
-const read = (state) => {
-  const { score, high, word, countdown } = state;
-  return (
-    <div className="app read">
-      {scoreBoard(score, high)}
-      <div>{word}</div>
-      <div>{countdown}</div>
-    </div>
-  );
-};
-
-const spell = (state, print, remove, giveUp) => {
-  const { score, high, choices, ans } = state;
-  return (
-    <div className="app spell">
-      {scoreBoard(score, high)}
-      <div>&nbsp;{ans}&nbsp;</div>
-      <div>
-        {choices.sort().map(letter => (
-          <button key={letter} onClick={() => print(letter)}>{letter}</button>
-        ))}
-        <button className="remove" onClick={remove}> &lt; </button>
-        <button className="give-up" onClick={giveUp}> ? </button>
+const getViews = (state) => ({
+  start: (start) => {
+    const { score, high, spelled } = state;
+    return (
+      <div className="app go">
+        {scoreBoard(score, high)}
+        <div>
+          <div>Get ready to spell!</div>
+          <div className="status">
+            {difference(words, spelled).length} of {words.length} words left to solve
+          </div>
+          <button className="reset" onClick={() => reset()}>Reset</button>
+        </div>
+        <div><button className="go" onClick={start}>Go</button></div>
       </div>
-    </div>
-  );
-};
-
-const yes = (state) => {
-  const { score, high, ans } = state;
-  return (
-    <div className="app yes">
-      {scoreBoard(score, high)}
-      <div>{ans}</div>
-      <div>Yes!!!</div>
-    </div>
-  )
-}
+    );
+  },
+  read: () => {
+    const { score, high, word, countdown } = state;
+    return (
+      <div className="app read">
+        {scoreBoard(score, high)}
+        <div>{word}</div>
+        <div>{countdown}</div>
+      </div>
+    );
+  },
+  spell: (print, remove, giveUp) => {
+    const { score, high, choices, ans } = state;
+    return (
+      <div className="app spell">
+        {scoreBoard(score, high)}
+        <div>&nbsp;{ans}&nbsp;</div>
+        <div>
+          {choices.sort().map(letter => (
+            <button key={letter} onClick={() => print(letter)}>{letter}</button>
+          ))}
+          <button className="remove" onClick={remove}> &lt; </button>
+          <button className="give-up" onClick={giveUp}> ? </button>
+        </div>
+      </div>
+    );
+  },
+  yes: () => {
+    const { score, high, ans } = state;
+    return (
+      <div className="app yes">
+        {scoreBoard(score, high)}
+        <div>{ans}</div>
+        <div>Yes!!!</div>
+      </div>
+    )
+  }
+});
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = initialState;
+    this.state = INITIAL_STATE;
+  }
+
+  update(newState, cb = () => {}) {
+    this.setState(prevState => ({
+      ...prevState,
+      ...newState,
+    }), cb);
   }
 
   componentDidMount() {
-    const high = window.localStorage?.getItem('speling-be') || 0;
+    const high = window.localStorage?.getItem(STORAGE_KEYS.high) || 0;
     if (high === null) {
-      window.localStorage.setItem('speling-be', 0);
+      window.localStorage.setItem(STORAGE_KEYS.high, 0);
     }
 
-    let spelled = window.localStorage?.getItem('spelled');
+    let spelled = window.localStorage?.getItem(STORAGE_KEYS.spelled);
     try {
       spelled = JSON.parse(spelled).map(x => x);
     } catch(e) {
       spelled = [];
     }
 
-    this.setState(prevState => ({
-      ...prevState,
-      high,
-      spelled,
-    }));
+    this.update({ high, spelled });
   }
 
-  spell() {
+  spell = () => {
     const { word } = this.state;
     const choices = uniq(word.split(''));
     while (choices.length < 15) {
@@ -115,80 +137,60 @@ class App extends React.Component {
       }
     }
 
-    this.setState(prevState => ({
-      ...this.state,
-      step: 2,
-      choices,
-    }));
+    this.update({ step: 2, choices });
   }
 
-  print(char) {
-    const guess = this.state.ans.concat(char);
-    const step = guess === this.state.word ? 3 : 2;
+  print = (char) => {
+    let { ans, word, score, high, spelled } = this.state;
+    const guess = ans.concat(char);
+    const step = guess === word ? 3 : 2;
 
     if (step === 3) {
-      setTimeout(() => {
-        this.setState(
-          prevState => {
-            const score = prevState.score + 1;
-            const high = score > prevState.high ? score : prevState.high;
-            const spelled = [...prevState.spelled, prevState.word];
-            window.localStorage.setItem('speling-be', high);
-            window.localStorage.setItem('spelled', JSON.stringify(spelled));
-            return {
-              ...prevState,
-              score,
-              high,
-              spelled,
-            };
-          },
-          () => this.start());
-      }, 2000);
+      score = score + 1;
+      high = score > high ? score : high;
+      spelled = [...spelled, word];
+      window.localStorage.setItem(STORAGE_KEYS.high, high);
+      window.localStorage.setItem(STORAGE_KEYS.spelled, JSON.stringify(spelled));
+      const update = () => this.update(
+        { score, high, spelled },
+        () => this.start()
+      );
+      setTimeout(update, 2000);
     }
 
-    this.setState(prevState => ({
-      ...prevState,
-      ans: guess,
-      step,
-    }));
+    this.update({ ans: guess, step });
   }
 
-  remove() {
-    this.setState(prevState => ({
-      ...prevState,
-      ans: this.state.ans.replace(/.$/, ''),
-    }));
+  remove = () => {
+    const { ans } = this.state;
+    this.update({ ans: ans.replace(/.$/, '') });
   }
 
-  giveUp() {
-    this.setState(prevState => ({
-      ...initialState,
-      score: 0,
-      high: prevState.high,
-      spelled: prevState.spelled,
-    }));
+  giveUp = () => {
+    const { high, spelled } = this.state;
+    this.update({ ...INITIAL_STATE, score: 0, high, spelled });
   }
 
-  start() {
-    this.setState(prevState => ({
-      ...prevState,
+  start = () => {
+    const { spelled } = this.state;
+    this.update({
       step: 1,
-      word: getWord(prevState.spelled),
+      word: getWord(spelled),
       ans: '',
       countdown: 3
-    }));
-    setTimeout(() => this.setState(prevState => ({ ...prevState, countdown: 2 })), 1000);
-    setTimeout(() => this.setState(prevState => ({ ...prevState, countdown: 1 })), 2000);
+    });
+    setTimeout(() => this.update({ countdown: 2 }), 1000);
+    setTimeout(() => this.update({ countdown: 1 }), 2000);
     setTimeout(() => this.spell(), 3000);
   }
 
   render() {
-    console.log(this.state.spelled);
+    const { start, read, spell, yes } = getViews(this.state);
     switch(this.state.step) {
-      case 0: return start(this.state, this.start.bind(this));
-      case 1: return read(this.state);
-      case 2: return spell(this.state, this.print.bind(this), this.remove.bind(this), this.giveUp.bind(this));
-      case 3: return yes(this.state);
+      case 0: return start(this.start);
+      case 1: return read();
+      case 2: return spell(this.print, this.remove, this.giveUp);
+      case 3: return yes();
     }
   }
 }
